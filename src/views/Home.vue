ba59b271e12a65379817edf75d860102a3d139fe
Asim Khan <khanasim034@gmail.com>
2019-12-12 00:32:11 +0500

    Login form with CORS Issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="login-form">
+      <input
+        v-model="email"
+        type="text"
+        class="form-control"
+        id="email"
+        placeholder="Please enter your email"
+      />
+
+      <password
+        v-model="password"
+        type="password"
+        class="form-control"
+        id="password"
+        width="auto"
+      />
+
+      <button @click="login" type="button" id="login" class="btn btn-success">
+        Login
+      </button>
+      <div id="response"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Password from "vue-password-strength-meter";
+import axios from "axios";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Password
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      const formData = new FormData();
+      formData.append("email", this.email);
+      formData.append("password", this.password);
+      let config = {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+        }
+      };
+      axios
+        .post("/signin", formData, config)
+        .then(response => {
+          console.log(response);
+        });
+    }
   }
 };
 </script>
+
+<style scoped>
+input {
+  background-color: #f1f1f1;
+  border: 1px solid #f1f1f1;
+  border-radius: 2px;
+  box-sizing: border-box;
+  font-size: 14px;
+  padding: 13px;
+  width: 400px;
+  margin-bottom: 8px;
+}
+.login-form {
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
